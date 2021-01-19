7090189a294c3f90a8379901cb6e5f709c70453b
Jhon Oliver <johnoliver021@gmail.com>
2021-01-19 13:21:46 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback, useEffect } from "react";
+import useBookSearch from "./useBookSearch";
 
-function App() {
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    console.log("pageNumber : ", pageNumber);
+  }, [pageNumber]);
+
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) {
+        // @ts-ignore: Object is possibly 'null'.
+        observer.current.disconnect();
+      }
+      // @ts-ignore: Object is possibly 'null'.
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      // @ts-ignore: Object is possibly 'null'.
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  function handleSearch(e: any) {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return (
+            <div ref={lastBookElementRef} key={book.key}>
+              {book.title}
+            </div>
+          );
+        } else {
+          return <div key={book.key}>{book.title}</div>;
+        }
+      })}
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error"}</div>
+    </>
   );
 }
-
-export default App;
